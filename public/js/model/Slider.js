dc560a7f3354f9bd77734d922a371d049019db89
codbear <douglas.meunier@codbear.com>
2019-05-26 10:14:22 +0200

    core: add a method to create html structure of the slider

--- public/js/model/Slider.js
+++ public/js/model/Slider.js
@@ -7,6 +7,25 @@
                 slidesToScroll: 1,
                 slidesVisible: 1
             }, userOptions);
+            this.createHtmlStructure();
+        }
+
+        createHtmlStructure() {
+            const userContainerChildren = [].slice.call(this.userContainer.children);
+            this.root = document.createElement('div');
+            this.root.classList.add('cdw-slider');
+            this.root.setAttribute('tabindex', '0');
+            this.container = document.createElement('div');
+            this.container.classList.add('cdw-slider-container');
+            this.root.appendChild(this.container);
+            this.userContainer.appendChild(this.root);
+            this.slides = userContainerChildren.map(child => {
+                let slide = document.createElement('div');
+                slide.classList.add('cdw-slide');
+                slide.appendChild(child);
+                this.container.appendChild(slide);
+                return slide;
+            })
         }
     }
 
