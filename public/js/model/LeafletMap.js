02f6b2fd50161873aaeb1535d3c4c73e77cb249f
codbear <douglas.meunier@codbear.com>
2019-05-27 23:34:46 +0200

    core(leafletmap): assign user's options

--- public/js/model/LeafletMap.js
+++ public/js/model/LeafletMap.js
@@ -1,8 +1,9 @@
 (function(window, document) {
     class LeafletMap {
 
-        constructor() {
-
+        constructor(tileLayer, attribution) {
+            this.tileLayer = tileLayer;
+            this.attribution = attribution;
         }
 
     }
