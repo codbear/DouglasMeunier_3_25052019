adb33c423934dafe11114463410745a577cf0218
codbear <douglas.meunier@codbear.com>
2019-05-27 23:43:25 +0200

    core(leafletmap): add a method to load the map

--- public/js/model/LeafletMap.js
+++ public/js/model/LeafletMap.js
@@ -2,8 +2,21 @@
     class LeafletMap {
 
         constructor(tileLayer, attribution) {
+            this.map = null;
             this.tileLayer = tileLayer;
             this.attribution = attribution;
+        }
+
+        loadMap(userContainer) {
+            return new Promise((resolve, reject) => {
+                window.$script('https://unpkg.com/leaflet@1.4.0/dist/leaflet.js', () => {
+                    this.map = L.map(userContainer);
+                    L.tileLayer(this.tileLayer, {
+                        attribution: '© ' + this.attribution
+                    }).addTo(this.map);
+                    resolve();
+                })
+            })
         }
 
     }
