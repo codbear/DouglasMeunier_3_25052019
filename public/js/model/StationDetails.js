4ca56f5aed5221537e53a0d53db96e6675977555
codbear <douglas.meunier@codbear.com>
2019-05-29 09:55:27 +0200

    core(stationdetails): add a method to set station's details

--- public/js/model/StationDetails.js
+++ public/js/model/StationDetails.js
@@ -12,6 +12,25 @@
             this.availableBikes = document.createElement('p');
             this.availableBikeStands = document.createElement('p');
         }
+
+        setDetails(station) {
+            switch (station.status) {
+                case 'OPEN':
+                    this.status.textContent = 'Ouvert';
+                    this.status.classList.add('green-text');
+                    break;
+                case 'CLOSED':
+                    this.status.textContent = 'Fermé';
+                    this.status.classList.add('red-text');
+                    break;
+                default :
+                    this.status.textContent = 'Impossible de récupérer l\'état de la station';
+            }
+            this.adress.textContent = station.adress.toLowerCase();
+            this.adress.classList.add('capitalize');
+            this.availableBikes.textContent = station.available_bikes + ' vélos dispônibles';
+            this.availableBikeStands.textContent = station.available_bike_stands + ' places disponibles';
+        }
     }
 
     window.StationDetails = StationDetails;
