f472241110e4ff9c0017a20e79276c7b8017e9ab
codbear <douglas.meunier@codbear.com>
2019-05-30 00:08:49 +0200

    core(modalbox): add a method to open the modal box

--- public/js/model/ModalBox.js
+++ public/js/model/ModalBox.js
@@ -4,6 +4,17 @@
         constructor() {
             this.modalBox = null;
         }
+
+        openBox(event) {
+            event.preventDefault();
+            this.modalBox = document.querySelector(event.target.dataset.target);
+            this.modalBox.style.display = null;
+            const closeBoxBound = this.closeBox.bind(this);
+            const stopPropagationBound = this.stopPropagation.bind(this);
+            this.modalBox.addEventListener('click', closeBoxBound);
+            this.modalBox.querySelector('.close-modal-box').addEventListener('click', closeBoxBound);
+            this.modalBox.querySelector('.modal-box-wrapper').addEventListener('click', stopPropagationBound);
+        }
     }
 
     window.ModalBox = ModalBox;
