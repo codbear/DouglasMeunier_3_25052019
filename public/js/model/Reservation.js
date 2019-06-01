bf0b9d3b9f514973da45def32458b3d4e13b933b
codbear <douglas.meunier@codbear.com>
2019-06-01 02:02:18 +0200

    core(Reservation): add a method to test if Web Storage is available

--- public/js/model/Reservation.js
+++ public/js/model/Reservation.js
@@ -5,6 +5,25 @@
             this.reservationForm = reservationForm;
         }
 
+        isStorageAvailable(type) {
+            try {
+                let storage = window[type],
+                    x = '__storage_test__';
+                storage.setItem(x, x);
+                storage.removeItem(x);
+                return true;
+            }
+            catch(e) {
+                return e instanceof DOMException && (
+                    e.code === 22 ||
+                    e.code === 1014 ||
+                    e.name === 'QuotaExceededError' ||
+                    e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                    storage.length !== 0;
+                )
+            }
+        }
+
         serializeIdentity() {
             return {firstName: this.reservationForm.firstName.value,
                     lastName: this.reservationForm.lastName.value}
